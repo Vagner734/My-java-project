8aa0cbe2dddef9c03d3d69a7dff5b8ccf05eea79
Vagner734 <vagnersantossouza734@gmail.com>
2024-10-28 23:32:01 -0300

    Add files via upload

---
+++ Turma.java
@@ -0,0 +1,73 @@
+package Atividade0001;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Turma {
+    
+    private String identificacao;
+    private Curso curso;
+    private int semestre;
+    private Professor professor;
+    private List<Estudante> estudantes;
+
+    public Turma() {
+        this.estudantes = new ArrayList<>();
+    }
+
+    public Turma(String identificacao) {
+        this.identificacao = identificacao;
+        this.estudantes = new ArrayList<>();
+    }
+
+    public String getIdentificacao() {
+        return identificacao;
+    }
+
+    public void setIdentificacao(String identificacao) {
+        this.identificacao = identificacao;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public List<Estudante> getEstudantes() {
+        return estudantes;
+    }
+
+    public void addEstudante(Estudante estudante) {
+        estudantes.add(estudante);
+    }
+
+    public void printTurmaDetails() {
+        System.out.println("Turma: " + identificacao + " - Curso: " + curso.getNome() + " - Semestre: " + semestre);
+        if (professor != null) {
+            professor.printData();
+        }
+        for (Estudante estudante : estudantes) {
+            estudante.printData();
+        }
+    }
+    
+}
