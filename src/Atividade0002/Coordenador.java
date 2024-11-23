340cf0f2b83daf8dd1ee2b94f88d475479c9d23e
Vagner734 <vagnersantossouza734@gmail.com>
2024-11-23 09:14:56 -0300

    Add files via upload

---
+++ src/Atividade0002/Coordenador.java
@@ -0,0 +1,8 @@
+package Atividade0002;
+
+// Classe Coordenador
+class Coordenador extends Professor {
+    public Coordenador(String nome, String cpf, String endereco, String telefone, int siape) {
+        super(nome, cpf, endereco, telefone, siape);
+    }
+}
