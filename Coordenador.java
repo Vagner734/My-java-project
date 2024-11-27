168d2c264ce26797ee7090891f4e43d4a00660bf
Vagner Souza <vagnersantossouza734@gmail.com>
2024-11-26 21:30:30 -0300

    Delete Coordenador.java

--- Coordenador.java
+++
@@ -1,6 +0,0 @@
-// Classe Coordenador
-public class Coordenador extends Professor {
-    public Coordenador(String nome, String cpf, String endereco, String telefone, int siape) {
-        super(nome, cpf, endereco, telefone, siape);
-    }
-}
