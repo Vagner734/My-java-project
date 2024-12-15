20b9d58f47defd44fcf697b39081fbd54968f79a
Vagner Souza <vagnersantossouza734@gmail.com>
2024-12-15 19:06:45 -0300

    Update Notas.java

--- src/Notas.java
+++ src/Notas.java
@@ -1,26 +1,39 @@
 public class Notas {
-    Double nota1;
-    Double nota2;
-    Double nota3;
-    Double peso1;
-    Double peso2;
-    Double peso3;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    private double peso1;
+    private double peso2;
+    private double peso3;
+    private Curso curso; // referência ao objeto Curso
 
-public double calcularMedia() { // "calcular":
-    if (nota1 == null) || (nota2 == null) || (nota3 == null) ) {
-        return -1; //ou lança uma execeção, dependendo da logica desejada 
-    }
-    double media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
-    return media;
+    // Construtor
+    public Notas(double nota1, double nota2, double nota3, double peso1, double peso2, double peso3, Curso curso) {
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.peso1 = peso1;
+        this.peso2 = peso2;
+        this.peso3 = peso3;
+        this.curso = curso;
     }
 
-public String getSituacao() { // "Situação":
-    double media = calcularMedia();
-    if (media >= curso.getMediaAprovacao()) {
-        return "APROVADO"; //"APROVADO"
-    } else if (media >= curso.getMediaRecuperacao()) { //"curso";
-         return "EM RECUPERACAO"; // "EM RECUPERACAO"
-    } else {
-        return "REPROVADO"; //"REPROVADO"
+    public double calcularMedia() { 
+        if (nota1 == 0 || nota2 == 0 || nota3 == 0) { // Verifica se as notas são zero
+            return -1; // ou lança uma exceção, dependendo da lógica desejada
+        }
+        double media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+        return media; // Adicionado ponto e vírgula
+    }
+
+    public String getSituacao() { 
+        double media = calcularMedia();
+        if (media >= curso.getMediaAprovacao()) {
+            return "APROVADO"; 
+        } else if (media >= curso.getMediaRecuperacao()) {
+            return "EM RECUPERAÇÃO"; 
+        } else {
+            return "REPROVADO"; 
+        }
     }
 }
